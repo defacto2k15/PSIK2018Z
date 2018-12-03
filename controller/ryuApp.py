b39a63aaae55e6b111bb1aceee03c0972c4b9e17
bartek <bczerwinski462@gmail.com>
2018-12-03 05:30:10 +0100

    Dziala dla recznego testu

--- controller/ryuApp.py
+++ controller/ryuApp.py
@@ -3,81 +3,354 @@ An OpenFlow 1.0 L2 learning switch implementation.
 """
 
 
+from collections import namedtuple
+
 from ryu.base import app_manager
 from ryu.controller import ofp_event
 from ryu.controller.handler import MAIN_DISPATCHER
 from ryu.controller.handler import set_ev_cls
-from ryu.ofproto import ofproto_v1_0
-from ryu.lib.mac import haddr_to_bin
-from ryu.lib.packet import packet
 from ryu.lib.packet import ethernet
-from ryu.lib.packet import ether_types
+from ryu.lib.packet import icmp
+from ryu.lib.packet import ipv4
+from ryu.lib.packet import packet
+from ryu.lib.packet import tcp
+from ryu.lib.packet.arp import arp
+from ryu.lib.packet.packet import Packet
+from ryu.ofproto import ether
+from ryu.ofproto import inet
+from ryu.ofproto import ofproto_v1_3
 from ryu.topology import event
 
+SWITCH3_DPID = 3
+SWITCH2_DPID = 2
+SWITCH1_DPID = 1
+
+OUTER_IP = "10.0.5.1"
+
+HOST_H1_S1_IP = "10.0.1.1"
+HOST_H1_S2_IP = "10.0.1.2"
+HOST_H2_S1_IP = "10.0.2.1"
+HOST_H2_S2_IP = "10.0.2.2"
+
+PORT_S3_K1 = 2
+PORT_S3_K2 = 4
+OUTER_PORTS = [PORT_S3_K1, PORT_S3_K2]
+
+PORT_S2_S3 = 3
+PORT_S2_H1 = 1
+PORT_S2_H2 = 2
+
+PORT_S1_H2 = 2
+PORT_S1_S3 = 3
+PORT_S1_H1 = 1
+
+MACADDR_S2_S3 = "00:00:00:00:00:04"
+MACADDR_S2_H1 = "00:00:00:00:00:03"
+MACADDR_S2_H2 = "00:00:00:00:00:02"
+
+MACADDR_S3_S2 = "00:00:00:00:00:05"
+MACADDR_H1_S2 = "00:00:00:00:00:01"
+MACADDR_H2_S2 = "00:00:00:00:00:22"
+
+MACADDR_S1_H2 = "00:00:00:00:00:21"
+MACADDR_S1_H1 = "00:00:00:00:00:23"
+MACADDR_H2_S1 = "00:00:00:00:00:10"
+
+MACADDR_S1_S3 = "00:00:00:00:00:09"
+MACADDR_S3_S1 = "00:00:00:00:00:08"
+MACADDR_H1_S1 = "00:00:00:00:00:25"
+
+
+INNER_PORTS = [1,3]
+
+global Switch_link
+Switch_link = namedtuple("Switch_link", ["port_id", "switch_port_mac", "other_port_mac"])
+
+INNER_CONECTIONS = [Switch_link(port_id=1, switch_port_mac="00:00:00:00:00:05", other_port_mac="00:00:00:00:00:04"),
+                    Switch_link(port_id=3, switch_port_mac="00:00:00:00:00:08", other_port_mac="00:00:00:00:00:09")]
+
+
+ROUTER_PORT1 = 1
+ROUTER_PORT2 = 2
+UINT32_MAX = 0xffffffff
+
+
+ETHERNET = ethernet.ethernet.__name__
+IPV4 = ipv4.ipv4.__name__
+ICMP = icmp.icmp.__name__
+
+DEFAULT_TTL = 64
 
 class SimpleSwitch(app_manager.RyuApp):
-    OFP_VERSIONS = [ofproto_v1_0.OFP_VERSION]
+    OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
+    global Ipv4_addr
+    Ipv4_addr = namedtuple("Ipv4_addr", ["addr", "port", "mac", "switch_port"])
+    global Switch_port
+    Switch_port = namedtuple("SwitchPort", ["port_mac", "port_id"])
 
     def __init__(self, *args, **kwargs):
         super(SimpleSwitch, self).__init__(*args, **kwargs)
+
         self.mac_to_port = {}
+
+        global maps
+        maps = {}
+        global ports
+        ports = range(50000, 60000)
 
     def add_flow(self, datapath, in_port, dst, src, actions):
         ofproto = datapath.ofproto
 
         match = datapath.ofproto_parser.OFPMatch(
+            eth_type=0x0800,
             in_port=in_port,
-            dl_dst=haddr_to_bin(dst), dl_src=haddr_to_bin(src))
+            eth_dst=dst, eth_src=src)
+
+        instructions = [datapath.ofproto_parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS, actions)]
 
         mod = datapath.ofproto_parser.OFPFlowMod(
             datapath=datapath, match=match, cookie=0,
             command=ofproto.OFPFC_ADD, idle_timeout=0, hard_timeout=0,
             priority=ofproto.OFP_DEFAULT_PRIORITY,
-            flags=ofproto.OFPFF_SEND_FLOW_REM, actions=actions)
+            flags=ofproto.OFPFF_SEND_FLOW_REM, instructions=instructions)
         datapath.send_msg(mod)
 
+    def add_flow2(self, datapath, match, actions, priority=0, hard_timeout=0):
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        mod = parser.OFPFlowMod(datapath=datapath, priority=priority, match=match, actions=actions,
+                                hard_timeout=hard_timeout, cookie=0, command=ofproto.OFPFC_ADD)
+        datapath.send_msg(mod)
+        self.logger.debug("add_flow:" + str(mod))
+
+
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
-    def _packet_in_handler(self, ev):
+    def packet_in_handler(self, ev):
         msg = ev.msg
         datapath = msg.datapath
-        ofproto = datapath.ofproto
+        inPort = msg.match['in_port']
+        packet = Packet(msg.data)
+        etherFrame = packet.get_protocol(ethernet.ethernet)
 
-        pkt = packet.Packet(msg.data)
-        eth = pkt.get_protocol(ethernet.ethernet)
+        if datapath.id == SWITCH3_DPID:
+            if etherFrame.ethertype == ether.ETH_TYPE_ARP:
+                self.logger.debug("Recieved at switch %s port %s ESrc %s EDst %s ARP", datapath.id, inPort, etherFrame.src, etherFrame.dst)
+                self.receive_arp(datapath, packet, etherFrame, inPort)
+                return 0
+            elif etherFrame.ethertype == ether.ETH_TYPE_IP:
+                ip_packet = packet.get_protocol(ipv4.ipv4)
+                self.logger.debug("Recieved at switch %s port %s ESrc %s EDst %s IP ISrc %s IDst %s Proto %s", datapath.id, inPort, etherFrame.src,
+                                  etherFrame.dst, ip_packet.src, ip_packet.dst, ip_packet.proto)
 
-        if eth.ethertype == ether_types.ETH_TYPE_LLDP:
-            # ignore lldp packet
-            return
-        dst = eth.dst
-        src = eth.src
+                if ip_packet.proto == 1: #ICMP
+                    icmp_packet = packet.get_protocol(icmp.icmp)
+                    self.reply_icmp(icmp_packet, ip_packet, etherFrame, inPort, datapath)
+                elif ip_packet.proto == 6: #TCP
+                    tcp_packet = packet.get_protocol(tcp.tcp)
+                    self.pass_tcp(  msg, tcp_packet, ip_packet, etherFrame)
+            else:
+                self.logger.debug("Drop packet")
+                return 1
+        elif datapath.id == SWITCH2_DPID or datapath.id == SWITCH1_DPID:
+            self.logger.info("SWITCH2 OR SWITCH1: packet in from switch2 or switch1")
+            ofproto = datapath.ofproto
 
-        dpid = datapath.id #datapath.id to identyfikator kontrollera
-        self.mac_to_port.setdefault(dpid, {})
+            pkt =  Packet(msg.data)
+            eth = pkt.get_protocol(ethernet.ethernet)
 
-        self.logger.info("packet in %s %s %s %s", dpid, src, dst, msg.in_port)
+            dst = eth.dst
+            src = eth.src
 
-        # learn a mac address to avoid FLOOD next time.
-        self.mac_to_port[dpid][src] = msg.in_port
+            dpid = datapath.id  # datapath.id to identyfikator kontrollera
+            self.mac_to_port.setdefault(dpid, {})
 
-        if dst in self.mac_to_port[dpid]:
-            out_port = self.mac_to_port[dpid][dst]
+            self.logger.info("packet in %s %s %s %s", dpid, src, dst, msg.match['in_port'])
+
+            # learn a mac address to avoid FLOOD next time.
+            self.mac_to_port[dpid][src] = msg.match['in_port']
+
+            if dst in self.mac_to_port[dpid]:
+                out_port = self.mac_to_port[dpid][dst]
+            else:
+                out_port = ofproto.OFPP_FLOOD
+
+            actions = [datapath.ofproto_parser.OFPActionOutput(out_port)]
+
+            # install a flow to avoid packet_in next time
+            if out_port != ofproto.OFPP_FLOOD:
+                self.add_flow(datapath, msg.match['in_port'], dst, src, actions)
+
+            data = None
+            if msg.buffer_id == ofproto.OFP_NO_BUFFER:
+                data = msg.data
+
+            out = datapath.ofproto_parser.OFPPacketOut(
+                datapath=datapath, buffer_id=msg.buffer_id, in_port=msg.match['in_port'],
+                actions=actions, data=data)
+            datapath.send_msg(out)
         else:
-            out_port = ofproto.OFPP_FLOOD
+            self.logger.info("Error: message from unexpected switch of datapath %s", datapath.id)
 
-        actions = [datapath.ofproto_parser.OFPActionOutput(out_port)]
 
-        # install a flow to avoid packet_in next time
-        if out_port != ofproto.OFPP_FLOOD:
-            self.add_flow(datapath, msg.in_port, dst, src, actions)
+    def reply_icmp(self, icmp_header, ipv4_header, ethernet_header, in_port, datapath):
+        self.send_icmp(in_port,
+                             icmp.ICMP_ECHO_REPLY,
+                             icmp.ICMP_ECHO_REPLY_CODE, datapath,
+                              ipv4_header, ethernet_header, icmp_data=icmp_header.data)
 
-        data = None
-        if msg.buffer_id == ofproto.OFP_NO_BUFFER:
-            data = msg.data
+    # z https://mik.bme.hu/~zfaigl/QoS/scripts/qos_rest_router.py
+    def send_icmp(self, in_port, icmp_type,
+                  icmp_code, datapath, ipv4_header=None, ethernet_header=None, icmp_data=None, msg_data=None, src_ip=None):
+        # Generate ICMP reply packet
+        csum = 0
+        offset = ethernet.ethernet._MIN_LEN
 
+        ether_proto = ether.ETH_TYPE_IP
+
+        eth = ethernet_header
+        e = ethernet.ethernet(eth.src, eth.dst, ether_proto)
+
+        if icmp_data is None and msg_data is not None:
+            ip_datagram = msg_data[offset:]
+            if icmp_type == icmp.ICMP_DEST_UNREACH:
+                icmp_data = icmp.dest_unreach(data_len=len(ip_datagram),
+                                              data=ip_datagram)
+            elif icmp_type == icmp.ICMP_TIME_EXCEEDED:
+                icmp_data = icmp.TimeExceeded(data_len=len(ip_datagram),
+                                              data=ip_datagram)
+
+        ic = icmp.icmp(icmp_type, icmp_code, csum, data=icmp_data)
+
+        ip = ipv4_header
+        if src_ip is None:
+            src_ip = ip.dst
+        ip_total_length = ip.header_length * 4 + ic._MIN_LEN
+        if ic.data is not None:
+            ip_total_length += ic.data._MIN_LEN
+            if ic.data.data is not None:
+                ip_total_length += + len(ic.data.data)
+        i = ipv4.ipv4(ip.version, ip.header_length, ip.tos,
+                      ip_total_length, ip.identification, ip.flags,
+                      ip.offset, DEFAULT_TTL, inet.IPPROTO_ICMP, csum,
+                      src_ip, ip.src)
+
+        pkt = packet.Packet()
+        pkt.add_protocol(e)
+
+        pkt.add_protocol(i)
+        pkt.add_protocol(ic)
+        pkt.serialize()
+
+        # Send packet out
+        self.send_packet_out(datapath, in_port, datapath.ofproto.OFPP_IN_PORT,
+                             pkt.data, data_str=str(pkt))
+
+
+    def receive_arp(self, datapath, packet, etherFrame, inPort):
+        arpPacket = packet.get_protocol(arp)
+
+        if arpPacket.opcode == 1:
+            arp_dstIp = arpPacket.dst_ip
+            self.reply_arp(datapath, etherFrame, arpPacket, arp_dstIp, inPort)
+        elif arpPacket.opcode == 2:
+            pass
+
+
+    def reply_arp(self, datapath, etherFrame, arpPacket, arp_dstIp, inPort):
+        dstIp = arpPacket.src_ip
+        srcIp = arpPacket.dst_ip
+        dstMac = etherFrame.src
+        our_mac = datapath.ports[inPort].hw_addr
+        if inPort in OUTER_PORTS and arp_dstIp == OUTER_IP:
+            srcMac = our_mac
+            outPort = inPort
+        elif inPort in  INNER_PORTS and srcIp == OUTER_IP:
+            srcMac = our_mac
+            outPort = inPort
+        else:
+            self.logger.debug("unknown arp requst received !")
+
+        self.send_arp(datapath, 2, srcMac, srcIp, dstMac, dstIp, outPort)
+
+    def send_arp(self, datapath, opcode, srcMac, srcIp, dstMac, dstIp, outPort):
+        targetMac = dstMac
+        targetIp = dstIp
+
+        e = ethernet.ethernet(dstMac, srcMac, ether.ETH_TYPE_ARP)
+        a = arp(1, 0x0800, 6, 4, opcode, srcMac, srcIp, targetMac, targetIp)
+        p = Packet()
+        p.add_protocol(e)
+        p.add_protocol(a)
+        p.serialize()
+
+        actions = [datapath.ofproto_parser.OFPActionOutput(outPort, 0)]
         out = datapath.ofproto_parser.OFPPacketOut(
-            datapath=datapath, buffer_id=msg.buffer_id, in_port=msg.in_port,
-            actions=actions, data=data)
+            datapath=datapath,
+            buffer_id=0xffffffff,
+            in_port=datapath.ofproto.OFPP_CONTROLLER,
+            actions=actions,
+            data=p.data)
         datapath.send_msg(out)
+
+    def pass_tcp(self, message, tcp_header, ip_header, ethernet_header):
+        datapath = message.datapath
+        in_port = message.match['in_port']
+        tcp_src_port = tcp_header.src_port
+        parser = datapath.ofproto_parser
+
+        if in_port in OUTER_PORTS: #from outer space
+            ipv4_addr = Ipv4_addr(addr=ip_header.src, port=tcp_src_port, mac=ethernet_header.src,
+                                  switch_port=Switch_port(port_mac=ethernet_header.dst, port_id=in_port))
+            if ipv4_addr in maps:
+                port = maps[ipv4_addr]
+            else:
+                port = ports.pop()
+                maps[ipv4_addr] = port
+                maps[port] = ipv4_addr
+                print "Created mapping: %s %s to %s %s" % (ipv4_addr.addr, ipv4_addr.port, OUTER_IP, port)
+
+            #TODO: lepszy wybor sciezki
+            inner_path = INNER_CONECTIONS[port % 2]
+
+            actions = [
+                parser.OFPActionSetField(eth_src=inner_path.switch_port_mac),
+                parser.OFPActionSetField( eth_dst=inner_path.other_port_mac, ),
+                parser.OFPActionSetField(ipv4_src=OUTER_IP),
+                parser.OFPActionSetField(tcp_src=port),
+                parser.OFPActionOutput(inner_path.port_id)
+            ]
+            data = None
+            # Check the buffer_id and if needed pass the whole message down
+            if message.buffer_id == 0xffffffff:
+                data = message.data
+            out = parser.OFPPacketOut(datapath=datapath, buffer_id=message.buffer_id, data=data,
+                                      in_port=in_port, actions=actions)
+            datapath.send_msg(out)
+        else:
+            dst_port = tcp_header.dst_port
+            if dst_port in maps:
+                ipv4_addr = maps[dst_port]
+            else:
+                print "Dropping msg as dst is not understood"
+                return
+            actions = [
+                parser.OFPActionSetField(eth_src=ipv4_addr.switch_port.port_mac),
+                parser.OFPActionSetField( eth_dst=ipv4_addr.mac),
+                parser.OFPActionSetField(ipv4_src=OUTER_IP),
+                parser.OFPActionSetField(ipv4_dst=ipv4_addr.addr),
+                parser.OFPActionSetField(tcp_dst=ipv4_addr.port),
+                parser.OFPActionOutput(ipv4_addr.switch_port.port_id)
+            ]
+
+            data = None
+            # Check the buffer_id and if needed pass the whole message down
+            if message.buffer_id == 0xffffffff:
+                data = message.data
+            out = parser.OFPPacketOut(datapath=datapath, buffer_id=message.buffer_id, data=data,
+                                      in_port=in_port, actions=actions)
+            datapath.send_msg(out)
+            return
 
     @set_ev_cls(ofp_event.EventOFPPortStatus, MAIN_DISPATCHER)
     def _port_status_handler(self, ev):
@@ -97,11 +370,65 @@ class SimpleSwitch(app_manager.RyuApp):
 
     @set_ev_cls(event.EventSwitchEnter)
     def switch_enter_handler(self, ev):
+        switch = ev.switch
+        datapath = switch.dp
 
-        switch = ev.switch.dp.id
-        self.logger.info("Entered %s", switch)
+        self.logger.info("Entered %s", datapath.id)
+        if datapath.id == SWITCH2_DPID:
+            self.logger.info("Proactively pushing rules to switch 2");
+            parser = datapath.ofproto_parser
+            ofproto = datapath.ofproto
+
+            buckets = [
+                parser.OFPBucket(
+                    weight=50,
+                    watch_port=PORT_S2_H1,
+                    watch_group=ofproto.OFPG_ANY,
+                    actions=[
+                        parser.OFPActionSetField(eth_dst=MACADDR_H1_S2),
+                        parser.OFPActionSetField( ipv4_dst=HOST_H1_S2_IP),
+                        parser.OFPActionOutput(PORT_S2_H1)]
+                ),
+                parser.OFPBucket(
+                    weight=50,
+                    watch_port=PORT_S2_H2,
+                    watch_group=ofproto.OFPG_ANY,
+                    actions=[
+                        parser.OFPActionSetField(eth_dst=MACADDR_H2_S2),
+                        parser.OFPActionSetField(ipv4_dst=HOST_H2_S2_IP),
+                        parser.OFPActionOutput(PORT_S2_H2)
+                    ]
+                ),
+            ]
+            group_id = 1
+            #req = parser.OFPGroupMod(datapath, ofproto.OFPGC_ADD, ofproto.OFPGT_SELECT, group_id, buckets)
+            #datapath.send_msg(req)
+
+            actions = [
+                parser.OFPActionSetField(eth_src=MACADDR_S2_H2),
+                parser.OFPActionSetField(eth_dst=MACADDR_H2_S2),
+                parser.OFPActionSetField(ipv4_dst=HOST_H2_S2_IP),
+                parser.OFPActionOutput(PORT_S2_H2)
+            ]
+            self.add_flow(datapath, PORT_S2_S3, MACADDR_S2_S3, MACADDR_S3_S2, actions )
+        elif datapath.id == SWITCH1_DPID:
+            self.logger.info("Proactively pushing rules to switch 1");
+            parser = datapath.ofproto_parser
+            actions = [
+                parser.OFPActionSetField(eth_src=MACADDR_S1_H1),
+                parser.OFPActionSetField(eth_dst=MACADDR_H1_S1),
+                parser.OFPActionSetField(ipv4_dst=HOST_H1_S1_IP),
+                parser.OFPActionOutput(PORT_S1_H1)
+            ]
+            self.add_flow(datapath, PORT_S1_S3, MACADDR_S1_S3, MACADDR_S3_S1, actions )
 
     @set_ev_cls(event.EventSwitchLeave, MAIN_DISPATCHER)
     def switch_leave_handler(self, ev):
         switch = ev.switch.dp.id
         self.logger.info("Left %s", switch)
+
+
+    def send_packet_out(self,datapath, in_port, output, data, data_str=None):
+        actions = [datapath.ofproto_parser.OFPActionOutput(output, 0)]
+        datapath.send_packet_out(buffer_id=UINT32_MAX, in_port=in_port,
+                                actions=actions, data=data)
--- mininet/mininetScript1.py
+++ mininet/mininetScript1.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from mininet.node import CPULimitedHost, DefaultController
 from mininet.topo import Topo
 from mininet.net import Mininet
@@ -22,83 +24,89 @@ class SimpleTopo(Topo):
         h1 = self.addHost('h1')
         h2 = self.addHost('h2')
 
-        s1 = self.addSwitch('s1', dpid='0000000000000221')
-        s2 = self.addSwitch('s2', dpid='0000000000000222')
-        s3 = self.addSwitch('s3', dpid='0000000000000223')  # switch-wtyczka miedzy siecia a kliantami
+        s1 = self.addSwitch('s1', dpid='0000000000000001', protocols='OpenFlow13')
+        s2 = self.addSwitch('s2', dpid='0000000000000002', protocols='OpenFlow13')
+        s3 = self.addSwitch('s3', dpid='0000000000000003', protocols='OpenFlow13')  # switch-wtyczka miedzy siecia a kliantami
 
         klient1 = self.addHost('klient1', ip="10.0.3.1/8")
         klient2 = self.addHost('klient2', ip="10.0.3.2/8")
 
-        self.addLink(h1, s1, intfName1='h1-eth-s1', intfName2='s1-eth-h1')
-        self.addLink(h1, s2, intfName1='h1-eth-s2', intfName2='s2-eth-h1')
+        #self.addLink(h1, s2, intfName1='h1-eth-s2', intfName2='s2-eth-h1', port1=1, port2=1)
+        self.addLink(h2, s2, intfName1='h2-eth-s2', intfName2='s2-eth-h2', port1=1, port2=2)
 
-        self.addLink(h2, s1, intfName1='h2-eth-s1', intfName2='s1-eth-h2')
-        self.addLink(h2, s2, intfName1='h2-eth-s2', intfName2='s2-eth-h2')
+        self.addLink(h1, s1, intfName1='h1-eth-s1', intfName2='s1-eth-h1', port1=2, port2=1)
+        #self.addLink(h2, s1, intfName1='h2-eth-s1', intfName2='s1-eth-h2', port1=2, port2=2)
 
-        self.addLink(klient1, s3, intfName1='k1-eth-s3', intfName2='s3-eth-k1')
-        self.addLink(klient2, s3, intfName1='k2-eth-s3', intfName2='s3-eth-k2')
+        self.addLink(klient1, s3, intfName1='k1-eth-s3', intfName2='s3-eth-k1', port1=1, port2 = 2)
+        self.addLink(klient2, s3, intfName1='k2-eth-s3', intfName2='s3-eth-k2', port1=1, port2=4)
 
-        # self.addLink(s1,s3, intfName1='my-s1-eth-s3', intfName2='my-s3-eth-s1')
-        # self.addLink(s2,s3, intfName1='my-s2-eth-s3', intfName2='my-s3-eth-s2')
-        self.addLink(s1, s3)
-        self.addLink(s2, s3)
+        self.addLink(s1, s3, port1=3, port2=3) #s1-eth3<->s3-eth3
+        self.addLink(s2, s3, port1=3, port2=1) #s2-eth3<->s3-eth4
 
 
 def configureNet(net):
-    h1, h2, s1, s2, s3, k1, k2 = net.get('h1', 'h2', 's1', 's2', 's3', 'klient1', 'klient2')
+    h1, h2, s3, s2, s1,  k1, k2 = net.get('h1', 'h2',  's3', 's2', 's1', 'klient1', 'klient2')
+
+    # h2.intf(intf='h2-eth-s1').setIP('10.0.2.1/8')
+    # h2.intf(intf='h2-eth-s1').setMAC('00:00:00:00:00:10')
+
+    # h1.intf(intf='h1-eth-s2').setIP('10.0.1.2/8')
+    # h1.intf(intf='h1-eth-s2').setMAC('00:00:00:00:00:02')
+
+    h2.intf(intf='h2-eth-s2').setIP('10.0.2.2/8')
+    h2.intf(intf='h2-eth-s2').setMAC('00:00:00:00:00:22')
 
     h1.intf(intf='h1-eth-s1').setIP('10.0.1.1/8')
-    h1.intf(intf='h1-eth-s1').setMAC('00:00:00:00:00:01')
-    h1.intf(intf='h1-eth-s2').setIP('10.0.1.2/8')
-    h1.intf(intf='h1-eth-s2').setMAC('00:00:00:00:00:02')
+    h1.intf(intf='h1-eth-s1').setMAC('00:00:00:00:00:25')
 
-    h2.intf(intf='h2-eth-s1').setIP('10.0.2.1/8')
-    h2.intf(intf='h2-eth-s1').setMAC('00:00:00:00:00:03')
-    h2.intf(intf='h2-eth-s2').setIP('10.0.2.2/8')
-    h2.intf(intf='h2-eth-s2').setMAC('00:00:00:00:00:04')
+    s3.intf(intf='s3-eth1').setMAC('00:00:00:00:00:05')
+    s3.intf(intf='s3-eth3').setMAC('00:00:00:00:00:08')
 
-    s1.intf(intf='s1-eth-h1').setMAC('00:00:00:00:00:05')
-    s1.intf(intf='s1-eth-h2').setMAC('00:00:00:00:00:06')
+    s2.intf(intf='s2-eth3').setMAC('00:00:00:00:00:04')
+    s1.intf(intf='s1-eth3').setMAC('00:00:00:00:00:09')
 
-    s2.intf(intf='s2-eth-h1').setMAC('00:00:00:00:00:07')
-    s2.intf(intf='s2-eth-h2').setMAC('00:00:00:00:00:08')
+    s3.intf(intf='s3-eth-k1').setMAC('00:00:00:00:00:06')
+    s3.intf(intf='s3-eth-k2').setMAC('00:00:00:00:00:03')
 
-    s3.intf(intf='s3-eth-k1').setMAC('00:00:00:00:00:09')
-    s3.intf(intf='s3-eth-k2').setMAC('00:00:00:00:00:10')
+    # s2.intf(intf='s2-eth-h1').setMAC('00:00:00:00:00:03')
+    s2.intf(intf='s2-eth-h2').setMAC('00:00:00:00:00:02')
+
+    # s1.intf(intf='s1-eth-h2').setMAC('00:00:00:00:00:21')
+    s1.intf(intf='s1-eth-h1').setMAC('00:00:00:00:00:23')
 
     k1.intf(intf='k1-eth-s3').setIP('10.0.3.1/8')
     k1.intf(intf='k1-eth-s3').setMAC('00:00:00:00:00:11')
-    k2.intf(intf='k2-eth-s3').setIP('10.0.4.1/8')
-    k2.intf(intf='k2-eth-s3').setMAC('00:00:00:00:00:12')
 
+    k2.intf(intf='k2-eth-s3').setIP('10.0.3.2/8')
+    k2.intf(intf='k2-eth-s3').setMAC('00:00:00:00:00:14')
 
 def run():
     myTopo = SimpleTopo()
-    # myController=DefaultController
     myController = partial(RemoteController, ip='192.168.74.5', port=6633)
     net = Mininet(topo=myTopo, host=CPULimitedHost,
                   controller=myController)
     configureNet(net)
     net.start()
-    net.pingAll()
 
-    # h1 = net.get('h1')
-    # h1.sendCmd('python testServer.py 10.0.1.1') To mi nie dziala
-
-    # A to mi działa... oczywiście, gdy nie ma SimpleHTTPServer, który zajmuje sockety
-    # h1 = net.get('h1')
-    # h1.sendCmd('python testServer.py 10.0.1.1')
-    # klient1 = net.get('klient1')
-    # klient1.sendCmd('python testClient.py 10.0.1.1 1')
-    # print('waiting for klient1 to send requests. Test results will be saved in \'log_1.txt\' file')
-    # klient1.waitOutput()
+    klient1 = net.get('klient1')
+    klient1.cmd('ping 10.0.5.1 -c1')
     
     h1 = net.get('h1')
-    h1.sendCmd('python -m SimpleHTTPServer 80 &') # To mi dziala
+    h1.cmd('cd /home/mininet/server/h1')
+    h1.cmd('python -m SimpleHTTPServer 80 &')
+
+    h2 = net.get('h2')
+    h2.cmd('cd /home/mininet/server/h2')
+    h2.cmd('python -m SimpleHTTPServer 80 &')
 
     CLI(net)
-    # potem w CLI uruchamiam polecenie klient2 wget -O - 10.0.1.1
 
+    # POLECENIA DO TESTOWANIA
+    # klient1 ping 10.0.5.1 -c1
+    # klient2 ping 10.0.5.1 -c1
+    #
+    # klient1 wget -O - -T 1 -t 1 10.0.5.1
+    # klient2 wget -O - -T 1 -t 1 10.0.5.1
     net.stop()
 
 
